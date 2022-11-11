71baab3044152da2aac4fa078aa59da8e603682d
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-11 17:19:11 +0100

    seed and show are ready

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,4 @@
 class Garden < ApplicationRecord
+  has_many :plants
   has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ require 'open-uri'
 puts "Cleaning database"
 
 
-Garden.destroy_all
-
 Plant.destroy_all
+
+Garden.destroy_all
 
 puts "creating 2 new gardens..with their plants"
 
@@ -30,8 +30,15 @@ file = URI.open('https://www.cmonjardinier.com/wp-content/uploads/2019/06/plante
 orchidée.photo.attach(io: file, filename: 'Orchidee/png', content_type: 'Orchidee.png')
 orchidée.save!
 
+rose = Plant.new(name: "Rose", garden_id: jardin_des_plantes.id)
+file = URI.open('https://cdn.shopify.com/s/files/1/0433/5448/8992/files/3_a127ffd9-8f73-4c47-9e85-b02c8ff5af14.jpg?v=1619169120')
+rose.photo.attach(io: file, filename: 'rose/jpeg', content_type: 'rose.jpeg')
+rose.save!
+
+
 puts "#{jardin_des_plantes.name} has been created!"
 puts "#{orchidée.name} has been created!"
+puts "#{rose.name} has been created"
 
 jardin_des_tuileries = Garden.new(
   name: "Jardin des Tuileries"
@@ -41,4 +48,16 @@ file = URI.open('https://www.radiofrance.fr/s3/cruiser-production/2022/02/dc890b
 jardin_des_tuileries.photo.attach(io: file, filename: 'jardindestuileries/jpg', content_type: 'jardindestuileries.jpg')
 jardin_des_tuileries.save!
 
+bambou = Plant.new(name: "Bambou", garden_id: jardin_des_tuileries.id)
+file = URI.open('https://www.alinea.com/dw/image/v2/BCKM_PRD/on/demandware.static/-/Sites-ali_master/default/dw234282d5/images/26478166/Plante-BAMBOU-26478166-A-1.jpg?sw=982&sh=982&sm=fit&sfrm=png')
+bambou.photo.attach(io: file, filename: 'bambou/jpeg', content_type: 'bambou.jpeg')
+bambou.save!
+
+pommier = Plant.new(name: "Pommier", garden_id: jardin_des_tuileries.id)
+file = URI.open('https://www.gammvert.fr/conseils/sites/default/files/styles/main_image/public/2017-09/fruitierreduit.jpg?itok=Ovw8Jzko')
+pommier.photo.attach(io: file, filename: 'Pommevert/jpeg', content_type: 'Pommier.jpeg')
+pommier.save!
+
 puts "#{jardin_des_tuileries.name} has been created!"
+puts "#{bambou.name} has been created!"
+puts "#{pommier.name} has been created!"
